55918a225a886d9443795c1d195c6f313cb802ba
shivasingh95 <thakurshivasinghraghuwanshi@gmail.com>
2026-07-19 00:00:32 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (67.89%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int a = nums[0];          
+        int b = nums.back();      
+        // Euclidean algorithm
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a; 
+    }
+};
