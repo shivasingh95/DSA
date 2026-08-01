c92c9ad94282a5291174bdb7420ad89684adfe85
shivasingh95 <thakurshivasinghraghuwanshi@gmail.com>
2026-08-01 19:19:56 +0530

    Time: 59 ms (19.54%) | Memory: 113.2 MB (61.09%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,36 @@
+class RandomizedSet {
+    vector<int> nums;
+    unordered_map<int, int> mp;
+
+public:
+    RandomizedSet() {}
+
+    bool insert(int val) {
+        if (mp.count(val)) return false;
+
+        nums.push_back(val);
+        mp[val] = nums.size() - 1;
+        return true;
+    }
+
+    bool remove(int val) {
+        if (!mp.count(val)) return false;
+
+        int idx = mp[val];
+        int last = nums.back();
+
+        // Move last element to idx
+        nums[idx] = last;
+        mp[last] = idx;
+
+        // Remove last element
+        nums.pop_back();
+        mp.erase(val);
+
+        return true;
+    }
+
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
